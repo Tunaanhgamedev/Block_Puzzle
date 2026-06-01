30fbeeeab5041497587835e5ea91dbf5e2086288
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-06-01 20:31:46 +0700

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Block_Puzzle/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
